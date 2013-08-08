8361071c8fd1d61d687b0c5d33f3cfc99f4222cc
Mike Lay <mike@mkly.io>
2013-08-07 17:11:51 -0700

    Additional Options

---
+++ README.md
@@ -0,0 +1,23 @@
+Usage: install-concrete5.php [OPTION]...
+Install concrete5 from the shell
+
+    --db-server=<hostname>             Location of database server
+    --db-username=<username>           Database username
+    --db-password=<password>           Database password
+    --db-database=<name>               Database name
+    --admin-email=<email>              Email of the admin user of the install
+    --admin-password=<password>        Password of the admin user of the install
+    --starting-point=<starting point>  Starting point to use
+    --target=<target location>         Target location of the install
+    --site=<site name>                 Name of the site
+    --core=<core location>             Location of the core concrete5 files
+    --reinstall=<no/yes>               If already installed at the target location
+                                       Delete current install and reinstall
+    --demo-username=<username>         Additional user username
+    --demo-password=<password>         Additional user password
+    --demo-email=<email>               Additional user email
+    --config=<file>                    Use configuration file for installation
+
+Requires minimum version 5.5.1 of concrete5
+Reports bugs to <https://github.com/concrete5/concrete5-cli>
+For use with the concrete5 CMS <http://www.concrete5.org>
--- install-concrete5.php
+++ install-concrete5.php
@@ -1,6 +1,35 @@
 #!/usr/bin/env php
 <?php
+$helpText = <<<EOT
+Usage: install-concrete5.php [OPTION]...
+Install concrete5 from the shell
 
+    --db-server=<hostname>             Location of database server
+    --db-username=<username>           Database username
+    --db-password=<password>           Database password
+    --db-database=<name>               Database name
+    --admin-email=<email>              Email of the admin user of the install
+    --admin-password=<password>        Password of the admin user of the install
+    --starting-point=<starting point>  Starting point to use
+    --target=<target location>         Target location of the install
+    --site=<site name>                 Name of the site
+    --core=<core location>             Location of the core concrete5 files
+    --reinstall=<no/yes>               If already installed at the target location
+                                       Delete current install and reinstall
+    --demo-username=<username>         Additional user username
+    --demo-password=<password>         Additional user password
+    --demo-email=<email>               Additional user email
+    --config=<file>                    Use configuration file for installation
+
+Requires minimum version 5.5.1 of concrete5
+Reports bugs to <https://github.com/concrete5/concrete5-cli>
+For use with the concrete5 CMS <http://www.concrete5.org>
+
+EOT;
+if (count($argv) === 1) {
+	echo $helpText;
+	exit;
+}
 
 define('FILE_PERMISSIONS_MODE', 0777);
 define('DIRECTORY_PERMISSIONS_MODE', 0777);
@@ -10,74 +39,63 @@ error_reporting(0);
 ini_set('display_errors', 0);
 define('C5_EXECUTE', true);
 
-foreach(array_slice($argv, 1) as $val) {
-	$val = explode('=', $val);
-	switch(current($val)) {
-		case '--db-server':
-			$DB_SERVER = next($val);
-			break;
-		case '--db-username':
-			$DB_USERNAME = next($val);
-			break;
-		case '--db-password':
-			$DB_PASSWORD = next($val);
-			break;
-		case '--db-database':
-			$DB_DATABASE = next($val);
-			break;
-		case '--admin-password':
-			$INSTALL_ADMIN_PASSWORD = next($val);
-			break;
-		case '--admin-email':
-			$INSTALL_ADMIN_EMAIL = next($val);
-			break;
-		case '--starting-point':
-			$INSTALL_STARTING_POINT = next($val);
-			break;
-		case '--target':
-			$target = next($val);
-			break;
-		case '--site':
-			$site = trim(next($val), '\'"'); //remove surrounding quotes (because site name likely has spaces in it, which need to be quoted in command-line args)
-			break;
-		case '--core':
-			$core = next($val);
-			break;
-		case '--reinstall':
-			$reinstall = true;
-			break;
-		case '--demo-username':
-			$DEMO_USERNAME = next($val);
-			break;
-		case '--demo-password':
-			$DEMO_PASSWORD = next($val);
-			break;
-		case '--demo-email':
-			$DEMO_EMAIL = next($val);
-			break;
+$defaults = array(
+	'db-server' => '',
+	'db-username' => '',
+	'db-password' => '',
+	'db-database' => '',
+	'admin-email' => 'admin@example.com',
+	'admin-password' => '',
+	'starting-point' => 'blank',
+	'target' => '',
+	'site' => 'concrete5 Site',
+	'core' => '',
+	'reinstall' => 'no',
+	'demo-username' => 'demo',
+	'demo-password' => '12345',
+	'demo-email' => 'demo@example.com'
+);
+
+$args = array();
+foreach (array_slice($argv, 1) as $arg) {
+	$opt = explode('=', $arg);
+	$args[str_replace('--', '', $opt[0])] = trim(isset($opt[1]) ? $opt[1] : '', '\'"');
+}
+if (array_key_exists('help', $args)) {
+	echo $helpText;
+	exit;
+}
+
+$config = array();
+if (array_key_exists('config', $args)) {
+	if (!file_exists($args['config'])) {
+		die("ERROR: Config file not found.\n");
 	}
+	include($args['config']);
 }
+$config = array_merge($defaults, $config, $args);
 
-if (!$INSTALL_STARTING_POINT) {
-	$INSTALL_STARTING_POINT = 'blank';
-}
-
-if (!empty($target)) {
+if ($config['target']) {
+	if (!file_exists($config['target'])) {
+		die("ERROR: Target location not found.\n");
+	}
 	if (substr($target, 0, 1) == '/') {
-		define('DIR_BASE', $target);
+		define('DIR_BASE', $config['target']);
 	} else { 
-		define('DIR_BASE', dirname(__FILE__) . '/' . $target);
+		define('DIR_BASE', dirname(__FILE__) . '/' . $config['target']);
 	}
 } else {
 	define('DIR_BASE', dirname(__FILE__));
 }
 
-if (!empty($core)) {
-	if (substr($core, 0, 1) == '/') {
-		$corePath = $core;	
+if ($config['core']) {
+	if (substr($config['core'], 0, 1) == '/') {
+		$corePath = $config['core'];
 	} else {
-		$corePath = dirname(__FILE__) . '/' . $core;
+		$corePath = dirname(__FILE__) . '/' . $config['core'];
 	}
+} elseif (file_exists(dirname(__FILE__) . '/' . 'install-concrete5-conf.php')) {
+		$corePath = dirname(__FILE__) . '/' . 'install-concrete5-conf.php';
 } else {
 	$corePath = DIR_BASE . '/concrete';
 }
@@ -87,7 +105,7 @@ if (!file_exists($corePath . '/config/version.php')) {
 	include($corePath . '/config/version.php');
 }
 
-if ($reinstall && is_file(DIR_BASE . '/config/site.php')) {
+if ($config['reinstall'] === 'yes' && is_file(DIR_BASE . '/config/site.php')) {
 	unlink(DIR_BASE . '/config/site.php');
 }
 if (file_exists(DIR_BASE . '/config/site.php')) {
@@ -133,7 +151,7 @@ require($corePath . '/config/theme_paths.php');
 
 ## Load session handlers
 require($corePath . '/startup/session.php');
-if ($reinstall) {
+if ($reinstall === 'yes') {
 	require($corePath . '/config/app.php');
 
 	// Remove all files from the files directory
@@ -149,8 +167,8 @@ if ($reinstall) {
 	removeDemoFiles(DIR_FILES_UPLOADED . '/');
 
 
-	foreach (Loader::db($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE)->MetaTables('TABLES') as $table) {
-		Loader::db($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE)->Execute('DROP TABLE '.$table);
+	foreach (Loader::db($config['db-server'], $config['db-username'], $config['db-password'], $config['db-database'])->MetaTables('TABLES') as $table) {
+		Loader::db($config['db-server'], $config['db-username'], $config['db-password'], $config['db-database'])->Execute('DROP TABLE '.$table);
 	}
 	Loader::library('cache');
 	Cache::flush();
@@ -182,19 +200,15 @@ if (is_object($fileWriteErrors)) {
 	$e->add($fileWriteErrors);
 }
 
-$_POST['SAMPLE_CONTENT'] = $INSTALL_STARTING_POINT;
-$_POST['DB_SERVER'] = $DB_SERVER;
-$_POST['DB_USERNAME'] = $DB_USERNAME;
-$_POST['DB_PASSWORD'] = $DB_PASSWORD;
-$_POST['DB_DATABASE'] = $DB_DATABASE;
-if (!empty($site)) {
-	$_POST['SITE'] = $site;
-} else {
-	$_POST['SITE'] = 'concrete5 Site';
-}
-$_POST['uPassword'] = $INSTALL_ADMIN_PASSWORD;
-$_POST['uPasswordConfirm'] = $INSTALL_ADMIN_PASSWORD;
-$_POST['uEmail'] = $INSTALL_ADMIN_EMAIL;
+$_POST['SAMPLE_CONTENT'] = $config['starting-point'];
+$_POST['DB_SERVER'] = $config['db-server'];
+$_POST['DB_USERNAME'] = $config['db-username'];
+$_POST['DB_PASSWORD'] = $config['db-password'];
+$_POST['DB_DATABASE'] = $config['db-database'];
+$_POST['SITE'] = $config['site'];
+$_POST['uPassword'] = $config['admin-password'];
+$_POST['uPasswordConfirm'] = $config['admin-password'];
+$_POST['uEmail'] = $config['admin-email'];
 
 if (version_compare($APP_VERSION, APP_VERSION_CLI_MINIMUM, '<')) {
 	$e->add('Your version of concrete5 must be at least ' . APP_VERSION_CLI_MINIMUM . ' to use this installer.');
@@ -215,7 +229,7 @@ if ($e->has()) {
 		print "ERROR: " . $ei . "\n";
 	}	
 } else {
-	$spl = Loader::startingPointPackage($INSTALL_STARTING_POINT);
+	$spl = Loader::startingPointPackage($config['starting-point']);
 	require(DIR_CONFIG_SITE . '/site_install.php');
 	require(DIR_CONFIG_SITE . '/site_install_user.php');
 	$routines = $spl->getInstallRoutines();
@@ -229,14 +243,14 @@ if ($e->has()) {
 		$cnt->reset();
 	}
 
-	if ($DEMO_USERNAME) {
+	if ($config['demo-username']) {
 		print "Adding demo user\n";
 		Loader::model('userinfo');
 		Loader::model('groups');
 		UserInfo::add(array(
-			'uName'            => $DEMO_USERNAME,
-			'uEmail'           => $DEMO_EMAIL,
-			'uPassword'        => $DEMO_PASSWORD
+			'uName'            => $config['demo-username'],
+			'uEmail'           => $config['demo-email'],
+			'uPassword'        => $config['demo-password']
 		))->getUserObject()->enterGroup(
 			Group::getByID(ADMIN_GROUP_ID)
 		);
